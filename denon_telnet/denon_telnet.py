fb26d2fc50c782775722fbd87d881a5222d8cbd4
Christoph Wagner <wgc.privat@gmail.com>
2014-01-24 23:22:34 +0100

    major rework to use start-stop-daemon

--- denon_telnet/denon_telnet.py
+++ denon_telnet/denon_telnet.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python2
 
 import sys, time
-from daemon import Daemon
 from telnet import MyDebugServerFactory, MyProxyClientFactory, MyProxyServerFactory
 from twisted.internet import reactor
 
-class TelnetMultiplexer(Daemon):
+class TelnetMultiplexer():
        
     #-----------------------------------------------------------#
     # Read in command line arguments
@@ -27,18 +26,5 @@ class TelnetMultiplexer(Daemon):
         reactor.run()
 
 if __name__ == "__main__":
-    daemon = TelnetMultiplexer('/tmp/daemon-example.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print "Unknown command"
-            sys.exit(2)
-            sys.exit(0)
-    else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
+    daemon = TelnetMultiplexer()
+    daemon.run()
